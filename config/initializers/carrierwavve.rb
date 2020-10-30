a5bbefd91151ca0079ccdc145f1d34eca7f570d9
keiki <keimaa0808@gmail.com>
2020-10-30 07:15:03 +0000

    編集

--- config/initializers/carrierwavve.rb
+++ config/initializers/carrierwavve.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = ENV['AWS_BUCKET']
+  config.fog_directory  = ENV['techstudy']
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
+    aws_access_key_id: ENV['AKIAUB6ZW3PGR4XRE55Q'],
+    aws_secret_access_key: ENV['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'],
+    region: ENV['ap-northeast-1'],
     path_style: true
   }
 end
